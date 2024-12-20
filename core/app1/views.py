7f3f63c7428527d47b5078fbf45eacf77731fc00
Rhainer <126873901+Rhainer13@users.noreply.github.com>
2024-12-20 14:54:11 +0800

    html changes
    -restricts updating when duplicate entry exists

--- core/app1/views.py
+++ core/app1/views.py
@@ -67,9 +67,21 @@ def update_resident(request, pk):
     if request.method == 'POST':
         form = ResidentForm(request.POST, instance=resident)
         if form.is_valid():
-            form.save()
-            return redirect('barangay-residents')
+            first_name = form.cleaned_data['first_name'].lower()
+            middle_name = form.cleaned_data['middle_name'].lower()
+            last_name = form.cleaned_data['last_name'].lower()
+            birth_date = form.cleaned_data['birth_date']
+            phone_number = form.cleaned_data['phone_number']
 
+            # Check for duplicate resident
+            if Resident.objects.filter(first_name=first_name, middle_name=middle_name, last_name=last_name, birth_date=birth_date).exclude(id=pk).exists():
+                messages.error(request, 'Resident with the same name and birth date already exists.')
+            elif Resident.objects.filter(phone_number=phone_number).exclude(id=pk).exists():
+                messages.error(request, 'Phone Number already exists.')
+            else:
+                form.save()
+                messages.success(request, f'Resident {first_name.capitalize()} {last_name.capitalize()} has been updated successfully.')
+                return redirect('barangay-residents')
     context = {
         'form':form,
     }
